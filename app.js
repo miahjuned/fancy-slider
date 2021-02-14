b11ea080ecc9bc6675eacb564324c73fc18b3304
zunead <zuned1996@outlook.com>
2021-02-14 22:32:19 +0600

    Fancy-Slider with Pixel API

---
+++ app.js
@@ -0,0 +1,161 @@
+const imagesArea = document.querySelector('.images');
+const gallery = document.querySelector('.gallery');
+const galleryHeader = document.querySelector('.gallery-header');
+const searchBtn = document.getElementById('search-btn');
+const sliderBtn = document.getElementById('create-slider');
+const sliderContainer = document.getElementById('sliders');
+const errorMassage = document.getElementById('errorMassage');
+// selected image 
+let sliders = [];
+
+
+// If this key doesn't work
+// Find the name in the url and go to their website
+// to create your own api key
+const KEY = '20269151-404055dda6d70ef88ef54d848';
+const getImages = (query) => {
+  Spinner();
+  gallery.innerHTML = ' ';
+  fetch(`https://pixabay.com/api/?key=${ KEY }&q=${ query }&image_type=photo&pretty=true`)
+    .then(response => response.json())
+    .then(data => showImages(data.hits))
+    .catch(err => {
+      errorMassage.innerHTML = `<h3>Something wrong! Try again!</h3>`;
+      Spinner();
+    })
+}
+
+// show images 
+const showImages = (images) => {
+  Spinner();
+  imagesArea.style.display = 'block';
+  gallery.innerHTML = '';
+  // show gallery title
+  galleryHeader.style.display = 'flex';
+  images.forEach(image => {
+    let div = document.createElement('div');
+    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${ image.webformatURL }") src="${ image.webformatURL }" alt="${ image.tags }">`;
+    gallery.appendChild(div);
+  });
+}
+
+let slideIndex = 0;
+const selectItem = (event, img) => {
+  let element = event.target;
+
+  let item = sliders.indexOf(img);
+  if (item === -1) {
+    sliders.push(img);
+    element.classList.toggle('added');
+  } else {
+    delete sliders[item];
+    element.classList.toggle('added');
+  }
+}
+var timer
+const createSlider = () => {
+  // check slider image length
+  if (sliders.length < 2) {
+    alert('Please Select at least 2 image.')
+    return;
+  }
+
+  const duration = document.getElementById('doration').value;
+  if (duration >= 500) {
+    // crate slider previous next area
+    sliderContainer.innerHTML = '';
+    const prevNext = document.createElement('div');
+    prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
+    prevNext.innerHTML = ` 
+  <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
+  <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
+  
+  `;
+
+    sliderContainer.appendChild(prevNext)
+    document.querySelector('.main').style.display = 'block';
+
+    // hide image aria
+    imagesArea.style.display = 'none';
+
+    sliders.forEach(slide => {
+      let item = document.createElement('div')
+      item.className = "slider-item";
+      item.innerHTML = `<img class="w-100 bg-warning"
+      src="${ slide }"
+      alt="">`;
+      sliderContainer.appendChild(item)
+    })
+    changeSlide(0)
+    timer = setInterval(function () {
+      slideIndex++;
+      changeSlide(slideIndex);
+    }, duration);
+  } else {
+    alert('If you are looking to create slider, then please type time duration number on the below box. Minimum time duration number: 500!')
+  }
+}
+
+// change slider index 
+const changeItem = index => {
+  changeSlide(slideIndex += index);
+}
+
+// change slide item
+const changeSlide = (index) => {
+
+  const items = document.querySelectorAll('.slider-item');
+  if (index < 0) {
+    slideIndex = items.length - 1
+    index = slideIndex;
+  };
+
+  if (index >= items.length) {
+    index = 0;
+    slideIndex = 0;
+  }
+
+  items.forEach(item => {
+    item.style.display = "none"
+  })
+
+  items[index].style.display = "block"
+}
+
+searchBtn.addEventListener('click', function () {
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  const search = document.getElementById('search');
+  getImages(search.value)
+  sliders.length = 0;
+})
+
+sliderBtn.addEventListener('click', function () {
+  createSlider();
+})
+
+document.getElementById("search").addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    document.getElementById("search-btn").click();
+  }
+});
+
+
+// Onloder
+var onloder;
+
+function onLoader() {
+  onloder = setTimeout(showPage, 1500);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+// spinner
+const Spinner = () => {
+  const spinner = document.getElementById('spinner');
+  spinner.classList.toggle("d-none");
+}
